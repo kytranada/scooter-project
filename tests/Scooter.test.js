8a70a9bc73b28ab29339a472c2fa87d1771e0930
Kofi B <kofi.boateng@verizon.com>
2024-09-25 15:35:58 -0400

    Finished Scooter tests

--- tests/Scooter.test.js
+++ tests/Scooter.test.js
@@ -1,21 +1,40 @@
 const Scooter = require('../src/Scooter')
 
-// typeof scooter === object
+
 describe('scooter object', () => {
+  const scooter = new Scooter()
+  
   test('Scooter class should create Scooter instance', () => {
-    const scooter = new Scooter()
     expect(scooter).toBeInstanceOf(Scooter)
+  })
+  test('Scooter properties are instantialized correctly', ()=> {
+    expect(scooter.user).toBe(null)
+    expect(scooter.serial).toBe(1)
+    expect(scooter.charge).toBe(100)
+    expect(scooter.isBroken).toBe(false)
   })
 })
 
+
 // Method tests
 describe('scooter methods', () => {
-  // tests here!
 
+  const scooter1 = new Scooter('Station 1')
+  let user  = {name : 'Ko'}
+  scooter1.rent(user)
+  
   // rent method
-
+  test('Rent method works', ()=> {
+    expect(scooter1.user).toBe(user)
+    expect(scooter1.station).toBe(null)
+  })
+ 
   // dock method
-
+  test('Dock method works', ()=> {
+    scooter1.dock('Station 2')
+    expect(scooter1.station).toBe('Station 2')
+    expect(scooter1.user).toBe(null)
+  })
   // requestRepair method
 
   // charge method
